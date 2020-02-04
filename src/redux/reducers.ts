b5d5d9c342128da2fb4270c6f482f15b131ff15c
Elena <lena@soapyfrog.com>
2020-02-04 14:57:42 +0000

    use async storage

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -2,6 +2,7 @@ import {AnyAction, combineReducers} from 'redux';
 import {AppState} from './store';
 
 export const CREATE_TASK = 'CREATE_TASK';
+export const RETRIEVE_STORED_TASKS = 'RETRIEVE_STORED_TASKS';
 
 const user = (state: AppState = {tasks: []}, _action: AnyAction) => {
   return state;
@@ -11,6 +12,11 @@ const tasks = (state = [], action: AnyAction) => {
   switch (action.type) {
     case CREATE_TASK:
       return [action.payload, ...state];
+    case RETRIEVE_STORED_TASKS:
+      console.log('::: RETRIEVE_STORED_TASKS [...action.payload] ::: ', [
+        ...action.payload,
+      ]);
+      return [...action.payload];
     default:
       return state;
   }
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,18 +1,45 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import {Form} from '../components/Form';
 import {AppState} from '../redux/store';
-import {TextOutput} from "../components/TextOutput";
+import {TextOutput} from '../components/TextOutput';
+import { CREATE_TASK, RETRIEVE_STORED_TASKS } from '../redux/reducers';
 
 interface AppProps {
   createTask: (task: string) => void;
+  setTasksFromAsyncStore: (tasks: Array<string>) => void;
   tasks: Array<string>;
 }
 
 const App = (props: AppProps) => {
   const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    async function retrieveAsyncStore() {
+      console.log(' --- retrieving async store');
+      const retrievedTasks = (await AsyncStorage.getItem('tasks')) || '{}';
+      props.setTasksFromAsyncStore(JSON.parse(retrievedTasks));
+    }
+    retrieveAsyncStore();
+  }, []);
+
+  useEffect(() => {
+    async function updateAsyncStore() {
+      try {
+        await AsyncStorage.setItem('tasks', JSON.stringify(props.tasks));
+      } catch (e) {
+        console.log(' e : ', e);
+      }
+    }
+    updateAsyncStore();
+    return function cleanup() {
+      console.log(' ::: will cleanup after unmounting ');
+    };
+  }, [props.tasks])
+
   const handleChange = (text: string) => {
     setNewTask(text);
   };
@@ -29,8 +56,6 @@ const App = (props: AppProps) => {
   );
 };
 
-const CREATE_TASK = 'CREATE_TASK';
-
 const mapStateToProps = (state: AppState) => {
   return {
     tasks: state.tasks,
@@ -43,6 +68,12 @@ const mapDispatchToProps = (dispatch) => {
       dispatch({
         type: CREATE_TASK,
         payload: task,
+      });
+    },
+    setTasksFromAsyncStore: (tasks: Array<string>) => {
+      dispatch({
+        type: RETRIEVE_STORED_TASKS,
+        payload: tasks,
       });
     },
   };
