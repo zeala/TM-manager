9e8ccf6119d73c80ec82bae620dafefc0afff3cd
Elena <lena@soapyfrog.com>
2020-02-05 13:08:47 +0000

    added vector icons

--- src/view/containers/Home.tsx
+++ src/view/containers/Home.tsx
@@ -3,6 +3,8 @@ import {Text, View} from 'react-native';
 import {connect} from 'react-redux';
 import {Form} from '../../components/Form';
 import {TextOutput} from '../../components/TextOutput';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 interface HomeProps {
   handleChange: (task: string) => void;
@@ -12,10 +14,13 @@ interface HomeProps {
 
 export const HomeComponent = (props: HomeProps) => {
   return (
-    <View>
+    <LinearGradient colors={['#ccc', '#f3c']} style={{height: '100%'}}>
       <Form handleChange={props.handleChange} handlePress={props.handlePress} />
       <TextOutput tasks={props.tasks} />
-    </View>
+      <Icon name="facebook" size={30} style={{width: 150, height: 50, position: 'absolute', top: '50%', alignSelf: 'center'}} color="blue">
+        <Text style={{fontSize: 12}}>Login with Facebook</Text>
+      </Icon>
+    </LinearGradient>
   );
 };
 
